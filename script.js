aa40252f91f585e793594a274580ed7a103ddfe6
Hari <harikrishnan53828@gmail.com>
2022-03-22 20:54:22 +0530

    f

---
+++ script.js
@@ -0,0 +1,48 @@
+ function calculate(){
+             var name = document.getElementById ("name-input").value;
+             var email = document.getElementById("email-input").value;
+             let contact = document.getElementById("num-input").value;
+             var school = document.getElementById("sch-input").value;
+             var college = document.getElementById("coll-input").value;
+
+             var certi = document.getElementById("certi-input").value;
+             var certi1 = document.getElementById("certi1-input").value;
+             var certi2 = document.getElementById("certi2-input").value;
+             var certi3 = document.getElementById("certi3-input").value;
+             var certi4 = document.getElementById("certi4-input").value;
+             var certi5 = document.getElementById("certi5-input").value;
+
+
+             var pro1 = document.getElementById("pro1-input").value;
+             var pro2 = document.getElementById("pro2-input").value;
+             var pro3 = document.getElementById("pro3-input").value;
+             var pro4 = document.getElementById("pro4-input").value;
+             var pro5 = document.getElementById("pro5-input").value;
+
+
+             document.getElementById("name-display").innerHTML = name ;
+             document.getElementById("email-display").innerHTML = email;
+             document.getElementById("num-display").innerHTML = contact ;
+             document.getElementById("sch-display").innerHTML = school ;
+             document.getElementById("coll-display").innerHTML = college ;
+
+
+             document.getElementById("certi-display").innerHTML =  certi;
+             document.getElementById("certi1-display").innerHTML =  certi1;
+             document.getElementById("certi2-display").innerHTML =  certi2;
+             document.getElementById("certi3-display").innerHTML =  certi3;
+             document.getElementById("certi4-display").innerHTML =  certi4;
+             document.getElementById("certi5-display").innerHTML =  certi5;
+
+
+
+             document.getElementById("pro1-display").innerHTML = pro1;
+             document.getElementById("pro2-display").innerHTML = pro2;
+             document.getElementById("pro3-display").innerHTML = pro3;
+             document.getElementById("pro4-display").innerHTML = pro4;
+             document.getElementById("pro5-display").innerHTML = pro5;
+
+             
+            }
+
+        
